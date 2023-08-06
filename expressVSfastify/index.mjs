28d360fd1de3b78ed3bddbb24708fad6215c58ec
cobragh <lucas_alvesf09@outlook.com>
2023-08-06 17:37:25 -0300

    Starting server w fastify

--- expressVSfastify/index.mjs
+++ expressVSfastify/index.mjs
@@ -1,10 +1,20 @@
-// STARTING A SERVER USING EXPRESS
-import express from "express";
-const app = express();
+// STARTING A SERVER USING E
+// import express from "express";
+// const app = express();
+// const port = 3000;
+// let start = performance.now();
+// app.listen(port, () => {
+//   console.log(`Server listening with express!`);
+// });
+// let end = performance.now();
+// console.log(`Execution timeStamp with Express: ${end - start} ms `);
+
+import Fastify from "fastify";
+const fastify = Fastify();
 const port = 3000;
 let start = performance.now();
-app.listen(port, () => {
-  console.log(`Server listening with express!`);
+fastify.listen({ port: port }, () => {
+  console.log(`Server listening with fastify!`);
 });
 let end = performance.now();
-console.log(`Execution timeStamp with Express: ${end - start} ms `);
+console.log(`Execution timeStamp with Fastify: ${end - start} ms `);
